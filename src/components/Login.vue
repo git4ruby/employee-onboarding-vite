05915bdc4409ae358c560472536702562d5edf91
Mohit Chandana <chandanamohit@gmail.com>
2023-05-31 15:56:12 -0400

    Enhance UI for login page

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,23 +1,36 @@
 <template>
-  <section>
-    <div class="flex justify-center items-center mt-52">
-      <div class="flex items-center justify-between">
-        <form @submit.prevent="login" class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
-          <div>
-            <label for="title" class="text-gray-700 text-sm font-bold mb-2">Email: </label>
-            <input type="text" id="email" v-model="postData.user.email">
-          </div>
-          <div class="flex items-center justify-between">
-            <label for="password" class="text-gray-700 text-sm font-bold mb-2">Password: </label>
-            <input type="password" id="password" v-model="postData.user.password">
-          </div>
-          <button class="bg-blue-600 hover:bg-black text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
-            Sign In
-          </button>
-        </form>
-      </div>
+  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+    <div class="sm:mx-auto sm:w-full sm:max-w-sm">
+      <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight">Sign in to your applica account</h2>
     </div>
-  </section>
+
+    <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+      <form class="space-y-6" @submit.prevent="login">
+        <div>
+          <label for="email" class="block text-sm font-medium leading-6">Email address</label>
+          <div class="mt-2">
+            <input id="email" name="email" type="email" autocomplete="email" required="" class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder: focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" v-model="postData.user.email" />
+          </div>
+        </div>
+
+        <div>
+          <div class="flex items-center justify-between">
+            <label for="password" class="block text-sm font-medium leading-6">Password</label>
+            <div class="text-sm">
+              <a href="#" class="font-semibold text-indigo-600 hover:text-indigo-500">Forgot password?</a>
+            </div>
+          </div>
+          <div class="mt-2">
+            <input id="password" name="password" type="password" autocomplete="current-password" required="" class="block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" v-model="postData.user.password" />
+          </div>
+        </div>
+
+        <div>
+          <button type="submit" class="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Sign in</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -36,10 +49,10 @@ export default {
   },
   methods: {
     login() {
-    axios
-      .post('http://localhost:4000/login', this.postData)
-      .then((response) => console.log(response))
-  }
+      axios
+        .post('http://localhost:4000/login', this.postData)
+        .then((response) => console.log(response))
+    }
   }
 }
 </script>
